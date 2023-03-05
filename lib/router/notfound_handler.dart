d5b71a7aebc025f3adc342fbee66bffdadecbcab
Julio C Tello <julioc.tello@me.com>
2023-03-05 00:59:43 -0600

    actualizacion de pagina de error

--- lib/router/notfound_handler.dart
+++ lib/router/notfound_handler.dart
@@ -1,4 +1,7 @@
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
+import 'package:solutica/providers/sidemenu_providers.dart';
+
 import 'package:solutica/ui/views/nopagefound_view.dart';
 
 
@@ -6,6 +9,9 @@ class NotFoundHandler {
 
   static Handler notFoundPage = Handler(
     handlerFunc: (context, params) {
+
+      Provider.of<SideMenuProvider>(context!, listen: false).currentPageUrl('/404');
+
       return const NoPageFound();
     }
     );
